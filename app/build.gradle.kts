ca161985d8271a07624c9e648cccab3b0e175ec1
Shubham <gargshubham2005@gmail.com>
2024-07-24 18:24:05 -0400

    Added ReportFragment that generates PDF

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,12 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    implementation("com.firebaseui:firebase-ui-storage:7.2.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
+    implementation ("com.squareup.picasso:picasso:2.8")
 }
