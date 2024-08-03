8d144c9d1d1f99d79b4c26b8b543289d3db47c93
devCodesApps <devoak11@gmail.com>
2024-08-03 19:50:31 -0400

    test

--- app/src/main/java/com/example/b07Project/MainActivity.java
+++ app/src/main/java/com/example/b07Project/MainActivity.java
@@ -20,12 +20,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        Button buttonAdmin = findViewById(R.id.buttonAdmin);
-        Button buttonBack = findViewById(R.id.buttonBack);
+        Button buttonAdmin = findViewById(R.id.button);
+        Button buttonBack = findViewById(R.id.back_button);
+        Button buttonRemove = findViewById(R.id.remove_button);
+        Button buttonAdd = findViewById(R.id.add_button);
+        Button buttonReport = findViewById(R.id.report_button);
+        Button buttonSearch = findViewById(R.id.search_button);
+
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,19 +55,29 @@ public class MainActivity extends AppCompatActivity {
                             .addToBackStack(null)
                             .commit();
                     buttonAdmin.setVisibility(View.GONE);
+                    buttonSearch.setVisibility(View.GONE);
                 }
 
             });
+        buttonAdd.setVisibility(View.GONE);
+        buttonReport.setVisibility(View.GONE);
+        buttonRemove.setVisibility(View.GONE);
+        buttonBack.setVisibility(View.GONE);
+        buttonSearch.setVisibility(View.VISIBLE);
 
         Log.d("isAdmin", Boolean.toString(AdminFragmentModel.isAdmin));
         if (AdminFragmentModel.isAdmin){
             buttonAdmin.setVisibility(View.GONE);
+            buttonAdd.setVisibility(View.VISIBLE);
+            buttonReport.setVisibility(View.VISIBLE);
+            buttonRemove.setVisibility(View.VISIBLE);
+            buttonBack.setVisibility(View.VISIBLE);
 
         }
 
     }
     public void back(View view) {
-
+        AdminFragmentModel.isAdmin = false;
         Intent intent = new Intent(MainActivity.this, MainActivity.class);
         startActivity(intent);
     }
